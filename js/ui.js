5beb70ffea3dd5d3797d7001bf96081857382bb2
Robert Fotino <robert.fotino@gmail.com>
2015-12-26 12:46:32 -0700

    Adds CodeMirror and drawer animations.

--- js/ui.js
+++ js/ui.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
     mainWindow = $('#main-window');
     gameCanvas = document.getElementById('game-canvas');
 
+    // Show/hide code and console windows when clicking their popout buttons
     $('#code-popout-btn').on('click', function() {
         codeHidden = !codeHidden;
         if (codeHidden) {
@@ -49,7 +50,6 @@ $(document).ready(function() {
             showCode();
         }
     });
-
     $('#console-popout-btn').on('click', function(e) {
         consoleHidden = !consoleHidden;
         if (consoleHidden) {
@@ -59,25 +59,37 @@ $(document).ready(function() {
         }
     });
 
+    // Start repositioning code and console windows on mousedown on
+    // their resize bars
     var codeDragging = false;
+    var consoleDragging = false;
     $('#code-dragbar').on('mousedown', function(e) {
         if (!codeHidden) {
             codeDragging = true;
+            codeWindow.addClass('notransition');
+            consoleWindow.addClass('notransition');
         }
     });
-
-    var consoleDragging = false;
     $('#console-dragbar').on('mousedown', function(e) {
         if (!consoleHidden) {
             consoleDragging = true;
+            consoleWindow.addClass('notransition');
         }
     });
-    
+
+    // Reposition the code and console windows when dragging their resize bars
     codePosition = Math.round($(window).width() * 0.5);
     consolePosition = Math.round($(window).height() * 0.65);
     $(document).on('mouseup', function(e) {
-        codeDragging = false;
-        consoleDragging = false;
+        if (codeDragging) {
+            codeDragging = false;
+            codeWindow.removeClass('notransition');
+            consoleWindow.removeClass('notransition');
+        }
+        if (consoleDragging) {
+            consoleDragging = false;
+            consoleWindow.removeClass('notransition');
+        }
     });
     $(document).on('mousemove', function(e) {
         if (codeDragging) {
@@ -90,6 +102,7 @@ $(document).ready(function() {
         }
     });
 
+    // Resize canvas, code window, and console window with the browser
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
     var resizeCanvas = function() {
@@ -105,6 +118,9 @@ $(document).ready(function() {
         windowWidth = $(window).width();
         windowHeight = $(window).height();
 
+        codeWindow.addClass('notransition');
+        consoleWindow.addClass('notransition');
+
         resizeCanvas();
 
         if (codeHidden) {
@@ -118,5 +134,23 @@ $(document).ready(function() {
         } else {
             showConsole();
         }
+
+        codeWindow[0].offsetHeight; // Flush cached CSS changes
+        codeWindow.removeClass('notransition');
+        consoleWindow.removeClass('notransition');
     });
+
+    // Set up CodeMirror in the coding window
+    var codeConfig = {
+        mode: 'javascript',
+        theme: 'elegant',
+        lineNumbers: true,
+        styleActiveLine: true,
+        matchBrackets: true
+    };
+    var cm = CodeMirror(function(elem) {
+        var textarea = document.getElementById('code');
+        textarea.parentNode.replaceChild(elem, textarea);
+        elem.id = 'code';
+    }, codeConfig);
 });
